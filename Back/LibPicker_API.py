90e12a52c2bf578b40a8e1135b36439aa4f7cba0
Ryan Saturno <ryanzig56@gmail.com>
2025-07-03 15:33:01 -0300

    Primeiro commit do projeto!

---
+++ Back/LibPicker_API.py
@@ -0,0 +1,22 @@
+import random
+from flask import Flask, jsonify, request
+from Back.steam_api_handler import *
+from dotenv import load_dotenv
+import os
+app = Flask(__name__) # Cria uma aplicação flask com o nome do arquivo atual.
+load_dotenv()
+steamid = os.getenv("steamid") # Pegando MEU steamid por enquanto
+
+#Sugerir um jogo aleatório que o usuário nunca jogou
+@app.route('/nuncajogados')
+def sugestao_nunca_jogado():
+    jogos = pegar_jogos(steamid)
+    jogos_filtrados = []
+    for jogo in jogos:
+        if jogo['playtime_forever'] == 0:
+            jogos_filtrados.append(jogo)
+    #Fazer verificação depois se existem jogos não jogados na biblioteca do usuário.
+    jogo_escolhido = random.choice(jogos_filtrados)
+    return jogo_escolhido
+
+app.run(port=5000,host='localhost',debug=True)
---
+++ Back/steam_api_handler.py
@@ -0,0 +1,22 @@
+import requests
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+KEY = os.getenv("KEY") # Chave da Steam API
+
+
+# Literalmente pegando os jogos da biblioteca do steamid selecionado via API steam.
+def pegar_jogos (steamid):
+    url = "http://api.steampowered.com/IPlayerService/GetOwnedGames/v0001/?key="+ KEY +"&steamid="+ steamid +"&format=json&include_appinfo=true"
+    resposta = requests.get(url) #Response != de dicionário
+    data = resposta.json() #Aqui tem o dicionário
+    #precisa verificar se a lista retornou alguma coisa também
+    if resposta.status_code == 200:
+        lista_jogos = data["response"].get("games") # Tratando o dict pegando só os jogos.
+        if lista_jogos is None:
+            return "Não foram encontrados jogos."
+        else:
+            return lista_jogos
+    else:
+        return "Ocorreu algum erro! tente novamente."
