46c874976b8619024120dfa447fe721af8d30f37
Ryan Saturno <ryanzig56@gmail.com>
2025-07-04 22:55:40 -0300

    Criação de respostas JSON para o Front-End

--- Back/LibPicker_API.py
+++ Back/LibPicker_API.py
@@ -3,9 +3,25 @@ from flask import Flask, jsonify, request
 from Back.steam_api_handler import *
 from dotenv import load_dotenv
 import os
+from datetime import datetime
 app = Flask(__name__) # Cria uma aplicação flask com o nome do arquivo atual.
 load_dotenv() # Carrega as variaveis de ambientes de teste para o código
 steamid = os.getenv("steamid") # Pegando MEU steamid por enquanto
+
+def montador_json(jogo):
+    data = datetime.fromtimestamp(jogo['rtime_last_played'])
+    data = data.strftime("%d/%m/%Y")
+    appid = str(jogo['appid'])
+    img_icon_url = str(jogo['img_icon_url'])
+    img_url_montada = "http://media.steampowered.com/steamcommunity/public/images/apps/{}/{}.jpg".format(appid, img_icon_url)
+    resposta_jogo = {
+        "nome_jogo": jogo['name'],
+        "tempo_de_jogo": jogo['playtime_forever'],
+        "ultima_vez_jogado": data,
+        "img_url_montada": img_url_montada,
+        "appid": jogo['appid']
+    }
+    return resposta_jogo
 
 #Sugerir um jogo aleatório que o usuário nunca jogou
 @app.route('/nuncajogados')
@@ -17,7 +33,7 @@ def sugestao_nunca_jogado():
             jogos_filtrados.append(jogo)
     #Fazer verificação depois se existem jogos não jogados na biblioteca do usuário.
     jogo_escolhido = random.choice(jogos_filtrados)
-    return jogo_escolhido
+    return montador_json(jogo_escolhido)
 #Sugerir um jogo com pouco playtime (até 10 horas)
 @app.route('/poucotempodejogo')
 def sugestao_poucotempodejogo():
@@ -28,14 +44,14 @@ def sugestao_poucotempodejogo():
         if jogo['playtime_forever'] > 0 and jogo['playtime_forever'] <= 600: # O playtime é medido em minutos
             jogos_filtrados.append(jogo)
     jogo_escolhido = random.choice(jogos_filtrados)
-    return jogo_escolhido
+    return montador_json(jogo_escolhido)
 
 #Sorteio dentre todos os jogos incluindo os grátis
 @app.route('/sorteiotodos')
 def sorteiotodos():
     jogos = pegar_jogos(steamid)
     jogo_escolhido = random.choice(jogos)
-    return jogo_escolhido
+    return montador_json(jogo_escolhido)
 
 @app.route('/maisjogados')
 def maisjogados():
@@ -43,10 +59,16 @@ def maisjogados():
     jogos_filtrados = []
     jogos.sort(key=lambda jogo: jogo['playtime_forever'], reverse=True)
     jogos_filtrados = jogos[:5]
-    return random.choice(jogos_filtrados)
+    jogo_escolhido = random.choice(jogos_filtrados)
+    return montador_json(jogo_escolhido)
 
-#app.route('/esquecidos')
+#@app.route('/esquecidos')
 #def esquecidos():
+#    jogos = pegar_jogos(steamid)
+#    jogos_filtrados = []
+#    data_atual = datetime.now()
+#    data_atual = data_atual.strftime("%d/%m/%Y")
+
 
 
 app.run(port=5000,host='localhost',debug=True)
