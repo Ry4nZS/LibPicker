e04c35cacf758dcc3ab191b628203e7d2281e8dc
Ryan Saturno <ryanzig56@gmail.com>
2025-07-06 18:22:23 -0300

    Integração com o Gemini feita!

--- Back/LibPicker_API.py
+++ Back/LibPicker_API.py
@@ -4,10 +4,19 @@ from Back.steam_api_handler import *
 from dotenv import load_dotenv
 import os
 from datetime import datetime, timedelta
+from google import genai
 
 app = Flask(__name__) # Cria uma aplicação flask com o nome do arquivo atual.
 load_dotenv() # Carrega as variaveis de ambientes de teste para o código
+client = genai.Client(api_key=os.getenv("GENAI_API_KEY")) # Carregando a chave de API do gemini
 steamid = os.getenv("steamid") # Pegando MEU steamid por enquanto
+
+def desc_jogo(nome_jogo):
+    resposta_gemini = client.models.generate_content(
+    model = "gemini-2.5-pro",
+    contents="Gere uma descrição breve e concisa de " + nome_jogo + "." + "Não passe de um parágrafo."
+    )
+    return resposta_gemini.text
 
 def montador_json(jogo):
     data = datetime.fromtimestamp(jogo['rtime_last_played'])
@@ -20,7 +29,8 @@ def montador_json(jogo):
         "tempo_de_jogo": jogo['playtime_forever'],
         "ultima_vez_jogado": data,
         "img_url_montada": img_url_montada,
-        "appid": jogo['appid']
+        "appid": jogo['appid'],
+        "descricao": desc_jogo(jogo['name'])
     }
     return resposta_jogo
 
@@ -35,6 +45,7 @@ def sugestao_nunca_jogado():
     #Fazer verificação depois se existem jogos não jogados na biblioteca do usuário.
     jogo_escolhido = random.choice(jogos_filtrados)
     return montador_json(jogo_escolhido)
+
 #Sugerir um jogo com pouco playtime (até 10 horas)
 @app.route('/poucotempodejogo')
 def sugestao_poucotempodejogo():
@@ -62,13 +73,14 @@ def maisjogados():
     jogos_filtrados = jogos[:5]
     jogo_escolhido = random.choice(jogos_filtrados)
     return montador_json(jogo_escolhido)
+
 @app.route('/esquecidos')
 def esquecidos():
     jogos = pegar_jogos(steamid)
     jogos_filtrados = []
     for jogo in jogos:
         ultima_vez_jogado = datetime.fromtimestamp(jogo['rtime_last_played'])
-        if ultima_vez_jogado <= datetime.now() - timedelta(days=180) and jogo['playtime_forever'] > 0:
+        if ultima_vez_jogado <= datetime.now() - timedelta(days=180) and jogo['playtime_forever'] > 300: #Seleciona um jogo que não foi jogado em 6 meses e tem mais de 5 horas de jogo
             jogos_filtrados.append(jogo)
     return montador_json(random.choice(jogos_filtrados))
 
